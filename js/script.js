baaae8ef73127aa8255ba2d7cb20c3c6ef766d05
Rifki Alhuraibi <riefky.alhuraibi@gmail.com>
2014-03-08 21:48:45 +0700

    redone everything

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(function() {
+    function command(cmd, term) {
+        cmd = cmd.toLowerCase();
+        switch (cmd) {
+            case "":
+                break;
+            case "help":
+                term.echo(
+                "Available command are:\n"+
+                "ABOUT\t\t Display all informations about me"+
+                "\n"
+                );
+                break;
+            case "about":
+                term.echo(
+                "\nHi! My name is Rifki Alhuraibi\n\n"+
+                "i'm a web developer from Surabaya Indonesia"+
+                "\n"
+                );
+                break;
+            default: 
+                term.error("command ["+cmd+"] not recognized");
+                break;
+        }
+    }
+    
+    $("#terminal").terminal(function(cmd, term){
+        command(cmd, term);
+    }, {
+        prompt : "$veelasky#>",
+        greetings:"Veelasky Console\n"+"Copyright (c) 2014 by Rifki Alhuraibi\n\n"+"type help to see all available command\n",
+    });
+});
